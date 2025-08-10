91137b3db3a52fd4f761fd6b23fb02b2be031ec6
digantgarude <digant.dg07@gmail.com>
2025-08-10 17:09:26 -0500

    Added api gateway logic and keycloak for security.

---
+++ apigateway/src/main/java/com/microservices/apigateway/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.microservices.apigateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    @Value("${keycloak.auth.jwk.set-uri}")
+    private String jwkSetUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+
+    }
+}
