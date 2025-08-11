572fcc8ef9aa4c8de4364bf6bed31724295fcfb9
digantgarude <digant.dg07@gmail.com>
2025-08-10 19:42:28 -0500

    Added Swagger Documentation for booking and inventory service.

--- apigateway/src/main/java/com/microservices/apigateway/config/SecurityConfig.java
+++ apigateway/src/main/java/com/microservices/apigateway/config/SecurityConfig.java
@@ -1,29 +1,41 @@
 package com.microservices.apigateway.config;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Arrays;
+
+@Slf4j
+@Configuration
 public class SecurityConfig {
     @Value("${keycloak.auth.jwk.set-uri}")
     private String jwkSetUri;
 
+    @Value("${security.excluded.urls}")
+    private String[] excludedUrls;
+
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        log.info(Arrays.toString(excludedUrls));
         return httpSecurity.authorizeHttpRequests(authorizeRequests ->
-                authorizeRequests.anyRequest().authenticated())
+                authorizeRequests
+                        .requestMatchers(excludedUrls)
+                        .permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
                 .build();
     }
 
     @Bean
     public JwtDecoder jwtDecoder() {
-
         return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
-
     }
 }
